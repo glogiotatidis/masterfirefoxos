c532b9a9d2902e56ec8c2ab193384583419607eb
Giorgos Logiotatidis <seadog@sealabs.net>
2015-02-02 14:05:11 +0200

    Upload db-strings.txt to S3.

--- bin/jenkins/makemessages.sh
+++ bin/jenkins/makemessages.sh
@@ -22,8 +22,9 @@ rm -rf locale db-strings.txt
 git clone $LOCALE_REPOSITORY locale
 
 deis login $DEIS_CONTROLLER --username $DEIS_USERNAME --password $DEIS_PASSWORD
-deis run -a $DEIS_APP -- "./manage.py runscript db_strings && cat db-strings.txt" > db-strings.txt
+deis run -a $DEIS_APP -- "./manage.py runscript db_strings && s3put -a \$AWS_ACCESS_KEY_ID -s \$AWS_SECRET_ACCESS_KEY -b \$AWS_STORAGE_BUCKET_NAME -g public-read  -p /app/ db-strings.txt"
 
+wget https://s3.amazonaws.com/masterfirefoxos-prod/db-strings.txt
 fig --project-name jenkins${JOB_NAME}${BUILD_NUMBER} run -T web ./manage.py makemessages -a --keep-pot
 fig --project-name jenkins${JOB_NAME}${BUILD_NUMBER} run -T web chmod a+wx -R locale
 
