d578369750df9e070a636a8177694b3876e757f7
Giorgos Logiotatidis <seadog@sealabs.net>
2015-02-19 15:25:28 +0200

    Per locale pot.

--- scripts/cleanup_po.py
+++ scripts/cleanup_po.py
@@ -1,4 +1,6 @@
+import os
 import re
+import shutil
 
 from django.conf import settings
 
@@ -28,34 +30,48 @@ def get_versions_for_locale(locale):
     return versions
 
 
-def run(*args):
-    for locale, lang_name in settings.LANGUAGES:
-        print('Processing locale {}'.format(locale))
-        versions = get_versions_for_locale(locale)
-        try:
-            po = polib.pofile('locale/{}/LC_MESSAGES/django.po'.format(locale))
-        except OSError:
-            print('Cannot open po file for locale {}'.format(locale))
+def cleanup_po(pofile, versions):
+    try:
+        po = polib.pofile(pofile)
+    except OSError:
+        print('Cannot open po file {}'.format(pofile))
+        return
+
+    to_remove = []
+    for entry in po:
+        # Currently unused string, keep.
+        if len(entry.occurrences) == 0:
             continue
 
-        to_remove = []
-        for entry in po:
-            # Currently unused string, keep.
-            if len(entry.occurrences) == 0:
-                continue
+        # String that appears in code, keep.
+        if code_string(entry.occurrences):
+            continue
 
-            # String that appears in code, keep.
-            if code_string(entry.occurrences):
-                continue
+        # String from documenation version valid for locale, keep.
+        if valid_version(entry.comment, versions):
+            continue
 
-            # String from documenation version valid for locale, keep.
-            if valid_version(entry.comment, versions):
-                continue
+        # Delete entry.
+        to_remove.append(entry)
 
-            # Delete entry.
-            to_remove.append(entry)
+    for entry in to_remove:
+        po.remove(entry)
 
-        for entry in to_remove:
-            po.remove(entry)
-        if to_remove:
-            po.save()
+    if to_remove:
+        po.save()
+
+
+def run(*args):
+    for locale, lang_name in settings.LANGUAGES:
+        versions = get_versions_for_locale(locale)
+        print('Processing locale {}'.format(locale))
+        pofile = 'locale/{}/LC_MESSAGES/django.po'.format(locale)
+        cleanup_po(pofile, versions)
+
+        print('Copy locale template')
+        pofile = 'locale/templates/{}/LC_MESSAGES/django.pot'.format(locale)
+        os.makedirs(os.path.dirname(pofile), exist_ok=True)
+        shutil.copy('locale/django.pot', pofile)
+
+        print('Processing locale {} template'.format(locale))
+        cleanup_po(pofile, versions)
